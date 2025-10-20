1ef375d0dd34eb7b3803bebb1a03609626a25bee
DeadlyParkour <deadlyparkour777@gmail.com>
2025-10-20 23:35:48 +0300

    added templates, root page, styles

--- qa/views.py
+++ qa/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
+
+QUESTIONS = [
+    {
+        'id': i,
+        'title': f'Вопрос номер {i}',
+        'text': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.',
+        'rating': random.randint(-10, 100),
+        'answers_count': random.randint(0, 20),
+        'tags': ['python', 'django', 'bootstrap']
+    } for i in range(30)
+]
+
+def index(request):
+    context = {'questions': QUESTIONS}
+    return render(request, 'index.html', context)
