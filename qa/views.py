3732e273f32dcdd13aa163a0639319d6a3871d99
DeadlyParkour <deadlyparkour777@gmail.com>
2025-12-25 17:30:03 +0300

    added vote views

--- qa/views.py
+++ qa/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
-from .models import Question, Answer, Tag
+from django.views.decorators.http import require_POST
+from django.http import JsonResponse
+from .models import Question, Answer, Tag, AnswerVote, QuestionVote
 from .utils import paginate
 from .forms import SignupForm, LoginForm, AskForm, SettingsForm
 from django.db.models import Count
@@ -102,3 +103,78 @@ def profile_edit(request):
         form = SettingsForm(request.user, initial=initial_data)
 
     return render(request, 'qa/settings.html', {'form': form})
+
+@require_POST
+@login_required
+def vote_question(request):
+    question_id = request.POST.get('question_id')
+    value = request.POST.get('value')
+    
+    question = get_object_or_404(Question, pk=question_id)
+    user = request.user
+    
+    if user.profile == question.author:
+        return JsonResponse({'error': 'Нельзя голосовать за свой вопрос'}, status=403)
+
+    vote, created = QuestionVote.objects.get_or_create(user=user, question=question)
+    
+    if value == 'like':
+        vote.value = 1
+    elif value == 'dislike':
+        vote.value = -1
+    else:
+        return JsonResponse({'error': 'Неверное действие'}, status=400)
+    
+    vote.save()
+    
+    rating = sum(v.value for v in QuestionVote.objects.filter(question=question))
+    question.rating = rating
+    question.save()
+    
+    return JsonResponse({'rating': rating})
+
+@require_POST
+@login_required
+def vote_answer(request):
+    answer_id = request.POST.get('answer_id')
+    value = request.POST.get('value')
+    
+    answer = get_object_or_404(Answer, pk=answer_id)
+    user = request.user
+
+    if user.profile == answer.author:
+        return JsonResponse({'error': 'Нельзя голосовать за свой ответ'}, status=403)
+
+    vote, created = AnswerVote.objects.get_or_create(user=user, answer=answer)
+    
+    if value == 'like':
+        vote.value = 1
+    elif value == 'dislike':
+        vote.value = -1
+    else:
+        return JsonResponse({'error': 'Неверное действие'}, status=400)
+    
+    vote.save()
+    
+    rating = sum(v.value for v in AnswerVote.objects.filter(answer=answer))
+    answer.rating = rating
+    answer.save()
+    
+    return JsonResponse({'rating': rating})
+
+@require_POST
+@login_required
+def mark_correct(request):
+    answer_id = request.POST.get('answer_id')
+    answer = get_object_or_404(Answer, pk=answer_id)
+    question = answer.question
+
+    if request.user.profile != question.author:
+        return JsonResponse({'error': 'Только автор вопроса может отмечать ответы'}, status=403)
+    
+    question.answer_set.update(is_correct=False)
+    
+    answer.is_correct = True
+    answer.save()
+    
+    return JsonResponse({'status': 'ok'})
